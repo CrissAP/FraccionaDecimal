90d9b0b1ab0fe4bb4253c5b4b281ba1ac065c12f
CrissAP <CrissAP@users.github.com>
2018-05-23 22:51:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pc/fraccionadecimal/IConvertidor.java
@@ -0,0 +1,13 @@
+package com.example.pc.fraccionadecimal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IConvertidor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iconvertidor);
+    }
+}
